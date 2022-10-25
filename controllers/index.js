34b1427f6cd960d115c05c41da8f0ac35132748c
H-meister <105766365+H-meister@users.noreply.github.com>
2022-10-24 21:18:51 -0400

    merge conflict

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-<<<<<<< HEAD
 
 router.use('/api', apiRoutes);
-=======
-const homeRoutes = require('./home-routes.js');
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-
->>>>>>> 22f861a2be9b5dca493aab6188e7473b8bcedab3
 //This is so if we make a request to any endpoint that doesn't exist, we'll receive a 404 error
 router.use((req, res) => {
     res.status(404).end();
