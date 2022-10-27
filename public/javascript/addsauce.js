1baa3b7eb38aa94d530da08d634bdac86c78d3a9
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-27 12:17:18 -0400

    worked on add suace js

--- public/javascript/addsauce.js
+++ public/javascript/addsauce.js
@@ -1,32 +1,42 @@
-const addSauce = document.querySelector('#submit').value.trim();
+const addNewSauce = document.getElementById('new-sauce')
+const addSauceDescription = document.getElementById('new-sauce-description')
+const addSauceLocation = document.getElementById('location')
+// const chooseHeat = document.getElementById('heat')
 
 async function submitNewSauce(event) {
     event.preventDefault();
 
-    const sauceName = document.querySelector('input[name="add-new-sauce"]').value;
-    const sauceDescription = document.querySelector('input[name="new-sauce-description"]').value;
+    //     const sauceName = document.querySelector('input[name="add-new-sauce"]').value;
+    //     const sauceDescription = document.querySelector('input[name="new-sauce-description"]').value;
 
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            sauceName,
-            sauceDescription
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    //     const response = await fetch(`/api/posts`, {
+    //         method: 'POST',
+    //         body: JSON.stringify({
+    //             sauceName,
+    //             sauceDescription
+    //         }),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+    //     if (response.ok) {
+    //         document.location.replace('/dashboard');
+    //     } else {
+    //         alert(response.statusText);
+    //     }
 };
 
-// addSauce.addEventListener('click', () => {
-//     console.log('clicking')
-//     document.location.replace('/dashboard/');
-// });
+addSauce.addEventListener('submit', () => {
+    let message = []
+    if (addNewSauce.value === '' || addSauceDescription.value === '' || addSauceLocation === '' || chooseHeat === '') {
+        message.push('name is required!')
+    }
+    else {
+        document.location.replace('/dashboard/');
+    }
 
-document.querySelector('.added-sauces').addEventListener('submit', submitNewSauce);
+    //document.location.replace('/dashboard/');
+});
+
+//document.querySelector('.added-sauces').addEventListener('submit', submitNewSauce);
