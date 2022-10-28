40a5c1ec27c183368d2e1e441247342158ea4e0f
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-28 13:13:19 -0400

    worked on add sauce page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Sauce  } = require('../../models');
+const { User, Post, Sauce } = require('../../models');
 
 //GET /api/users
 router.get('/', (req, res) => {
@@ -45,45 +45,45 @@ router.post('/', (req, res) => {
         password: req.body.password,
         name: req.body.name
     })
-    .then(dbUserData => {
-        req.session.save(() => {
-          req.session.user_id = dbUserData.id;
-          req.session.username = dbUserData.username;
-          req.session.loggedIn = true;
-      
-          res.json(dbUserData);
-        });
-      })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
 });
 
 //POST login! api/users/login
 router.post('/login', (req, res) => {
     User.findOne({
-      where: {
-        email: req.body.email
-      }
+        where: {
+            email: req.body.email
+        }
     }).then(dbUserData => {
-      if (!dbUserData) {
-        res.status(400).json({ message: 'No user with that email address!' });
-        return;
-      }
-  
-      const validPassword = dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res.status(400).json({ message: 'Incorrect password!' });
-        return;
-      }
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
 
-      req.session.save(() => {
-        //declare session variables
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-      })
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            //declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        })
     });
-  });
+});
 //PUT /api/users/1
 router.put('/:id', (req, res) => {
 
--- public/javascript/addsauce.js
+++ public/javascript/addsauce.js
@@ -1,42 +1,30 @@
-const addNewSauce = document.getElementById('new-sauce')
-const addSauceDescription = document.getElementById('new-sauce-description')
-const addSauceLocation = document.getElementById('location')
-// const chooseHeat = document.getElementById('heat')
-
-async function submitNewSauce(event) {
+async function addSauceFormHandler(event) {
     event.preventDefault();
 
-    //     const sauceName = document.querySelector('input[name="add-new-sauce"]').value;
-    //     const sauceDescription = document.querySelector('input[name="new-sauce-description"]').value;
+    //const newSauce = document.querySelector('#new-sauce').value.trim();
+    const sauceDescription = document.querySelector('#new-sauce-description').value.trim();
+    const location = document.querySelector('#location').value.trim();
+    const heatLevel = document.querySelector('#heat-level').value.trim();
 
-    //     const response = await fetch(`/api/posts`, {
-    //         method: 'POST',
-    //         body: JSON.stringify({
-    //             sauceName,
-    //             sauceDescription
-    //         }),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     });
 
-    //     if (response.ok) {
-    //         document.location.replace('/dashboard');
-    //     } else {
-    //         alert(response.statusText);
-    //     }
+    if (sauceDescription && location && heatLevel) {
+        const response = await fetch('/api/post/addsauce', {
+            method: 'post',
+            body: JSON.stringify({
+                //name,
+                description,
+                location,
+                sco_score
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Fill out all required feilds!');
+        }
+    }
 };
 
-addSauce.addEventListener('submit', () => {
-    let message = []
-    if (addNewSauce.value === '' || addSauceDescription.value === '' || addSauceLocation === '' || chooseHeat === '') {
-        message.push('name is required!')
-    }
-    else {
-        document.location.replace('/dashboard/');
-    }
-
-    //document.location.replace('/dashboard/');
-});
-
-//document.querySelector('.added-sauces').addEventListener('submit', submitNewSauce);
+document.querySelector('.card').addEventListener('submit', addSauceFormHandler);
