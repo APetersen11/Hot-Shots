8423843adbc3cfc04f73652e600e88e29ef88811
Alyssa Petersen <aptrsn11@gmail.com>
2022-10-30 17:43:19 -0400

    more work on button

--- public/javascript/addsauce.js
+++ public/javascript/addsauce.js
@@ -4,38 +4,41 @@ console.log(cloudinary.config().cloud_name);
 
 // add functionality to widget
 async function addSauceFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    //const newSauce = document.querySelector('#new-sauce').value.trim();
-    const sauceDescription = document.querySelector('#new-sauce-description').value.trim();
-    const location = document.querySelector('#location').value.trim();
-    const heatLevel = document.querySelector('#heat-level').value.trim();
+  //const newSauce = document.querySelector('#new-sauce').value.trim();
+  const sauceDescription = document
+    .querySelector("#new-sauce-description")
+    .value.trim();
+  const location = document.querySelector("#location").value.trim();
+  const heatLevel = document.querySelector("#heat-level").value.trim();
 
+  if (sauceDescription && location && heatLevel) {
+    const response = await fetch("/api/post/addsauce", {
+      method: "post",
+      body: JSON.stringify({
+        //name,
+        description,
+        location,
+        sco_score,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-    if (sauceDescription && location && heatLevel) {
-        const response = await fetch('/api/post/addsauce', {
-            method: 'post',
-            body: JSON.stringify({
-                //name,
-                description,
-                location,
-                sco_score
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Fill out all required fields!');
-        }
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Fill out all required fields!");
     }
-};
+  }
+}
 
 // add sauce button
-addSauce.addEventListener('submit', () => {
-    console.log('clicking')
-    document.location.replace('/dashboard/');
+addSauce.addEventListener("submit", () => {
+  console.log("clicking");
+  document.location.replace("/dashboard/");
 });
 
-document.querySelector('.added-sauces').addEventListener('submit', submitNewSauce);
+document
+  .querySelector(".added-sauces")
+  .addEventListener("submit", addSauceFormHandler);
