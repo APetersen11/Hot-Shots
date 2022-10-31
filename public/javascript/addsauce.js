a50d089d35b7b785b7073ba8230707ec539ae2a4
Alyssa Petersen <aptrsn11@gmail.com>
2022-10-31 18:42:43 -0400

    fix add sauce form submission

--- public/javascript/addsauce.js
+++ public/javascript/addsauce.js
@@ -1,30 +1,35 @@
+console.log("yo, I am working");
+
 async function addSauceFormHandler(event) {
   event.preventDefault();
 
-    const name = document.querySelector('#sauce_name').value.trim();
-    const description = document.querySelector('#new-sauce-description').value.trim();
-    const location = document.querySelector('#location').value.trim();
-    const sco_score = document.querySelector('#heat-level').value.trim();
+  const name = document.querySelector("#sauce_name").value.trim();
+  const description = document
+    .querySelector("#new-sauce-description")
+    .value.trim();
+  const location = document.querySelector("#location").value.trim();
+  const sco_score = document.querySelector("#heat-level").value.trim();
 
+  if (name && description && location && sco_score) {
+    const response = await fetch("/api/addsauce", {
+      method: "post",
+      body: JSON.stringify({
+        name,
+        description,
+        location,
+        sco_score,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-    if (name && description && location && sco_score) {
-        const response = await fetch('/api/addsauce', {
-            method: 'post',
-            body: JSON.stringify({
-                name,
-                description,
-                location,
-                sco_score
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Fill out all required fields!');
-        }
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Fill out all required fields!");
     }
-};
+  }
+}
 
-document.querySelector('.addsauce-form').addEventListener('submit', addSauceFormHandler);
+document
+  .querySelector(".addsauce-form")
+  .addEventListener("submit", addSauceFormHandler);
