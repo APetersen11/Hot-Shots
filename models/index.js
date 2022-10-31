55c6a983f49bee4a4e85c928e5e00e636a034895
Alyssa Petersen <aptrsn11@gmail.com>
2022-10-31 10:14:43 -0400

    merge branches

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 // import all models
 const User = require('./user');
-const Post = require('./post');
 const Sauce = require('./sauce');
 
-module.exports = { User, Post, Sauce };
+module.exports = { User, Sauce };
 
 
 
--- public/javascript/addsauce.js
+++ public/javascript/addsauce.js
@@ -1,44 +1,66 @@
-require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
-console.log(cloudinary.config().cloud_name);
 
 // add functionality to widget
 async function addSauceFormHandler(event) {
   event.preventDefault();
 
-  //const newSauce = document.querySelector('#new-sauce').value.trim();
-  const sauceDescription = document
-    .querySelector("#new-sauce-description")
-    .value.trim();
-  const location = document.querySelector("#location").value.trim();
-  const heatLevel = document.querySelector("#heat-level").value.trim();
+    const name = document.querySelector('#sauce_name').value.trim();
+    const description = document.querySelector('#new-sauce-description').value.trim();
+    const location = document.querySelector('#location').value.trim();
+    const sco_score = document.querySelector('#heat-level').value.trim();
 
-  if (sauceDescription && location && heatLevel) {
-    const response = await fetch("/api/post/addsauce", {
-      method: "post",
-      body: JSON.stringify({
-        //name,
-        description,
-        location,
-        sco_score,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
 
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert("Fill out all required fields!");
+    if (name && description && location && sco_score) {
+        const response = await fetch('/api/sauce_post', {
+            method: 'post',
+            body: JSON.stringify({
+                name,
+                description,
+                location,
+                sco_score
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            windows.alert('Fill out all required feilds!');
+        }
     }
-  }
-}
+};
 
-// add sauce button
-addSauce.addEventListener("submit", () => {
-  console.log("clicking");
-  document.location.replace("/dashboard/");
-});
+// document.getElementById("app").innerHTML = (
+//     <div>
+//       <button id="upload_widget" class="cloudinary-button">
+//         Upload Image
+//       </button>
+//     </div>
+//   );
+  
+//   var myWidget = cloudinary.createUploadWidget(
+//     {
+//       cloudName: "dwjycofwm",
+//       uploadPreset: "hs_test",
+//     },
+//     (error, result) => {
+//       if (!error && result && result.event === "success") {
+//         console.log("Done! Here is the image info: ", result.info);
+//       }
+//     }
+//   );
+  
+//   document.getElementById("upload_widget").addEventListener(
+//     "click",
+//     function () {
+//       myWidget.open();
+//     },
+//     false
+//   );
 
-document
-  .querySelector(".added-sauces")
-  .addEventListener("submit", addSauceFormHandler);
+// addSauce.addEventListener('click', () => {
+//     console.log('clicking')
+//     document.location.replace('/dashboard/');
+// });
+
+document.querySelector('.addsauce-form').addEventListener('submit', addSauceFormHandler);
