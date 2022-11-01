8abf1be83eb2768c6a1b58007134e1cc3d1ba89c
H-meister <105766365+H-meister@users.noreply.github.com>
2022-10-31 20:48:55 -0400

    adding login functionality back running"

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,36 +6,36 @@ router.get('/', (req, res) => {
 });
 //login if not logged in redirect
 router.get('/login', (req, res) => {
-    // if (req.session.loggedIn) {
-    //     res.redirect('/dashboard');
-    //     return;
-    // }
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
 
     res.render('login');
 });
 //sign up session authentication
 router.get('/signup', (req, res) => {
-    // if (req.session.loggedIn) {
-    //     res.redirect('/dashboard');
-    //     return;
-    // }
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
 
     res.render('signup');
 });
 //redirect if tried to access without loggin in
 router.get('/addsauce', (req, res) => {
-    // if (!req.session.loggedIn) {
-    //     res.redirect('/login');
-    //     res.status(401);
-    // }
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+        res.status(401);
+    }
     res.render('addsauce');
 });
 //same here
 router.get('/dashboard', (req, res) => {
-    // if (!req.session.loggedIn) {
-    //     res.redirect('/login');
-    //     res.status(401);
-    // }
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+        res.status(401);
+    }
     res.render('dashboard');
 });
 
